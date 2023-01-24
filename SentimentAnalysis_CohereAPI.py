0113a549ed84871d6b0ac396eaf7aab89bdbfd3f
VilakshanKh <31506996+Vilakshankh@users.noreply.github.com>
2023-01-24 00:23:21 -0330

    Create SentimentAnalysis_CohereAPI.py

---
+++ SentimentAnalysis_CohereAPI.py
@@ -0,0 +1,37 @@
+import cohere
+
+from cohere.classify import Example
+
+co = cohere.Client('KYicB5XGH8vpEBuSK3xlpzMyzAH5OKtPTdyeloPR')
+
+examples = [
+    Example("The order came 5 days early", "positive"), 
+    Example("The item exceeded my expectations", "positive"), 
+    Example("I ordered more for my friends", "positive"), 
+    Example("I would buy this again", "positive"), 
+    Example("I would recommend this to others", "positive"), 
+    Example("The package was damaged", "negative"), 
+    Example("The order is 5 days late", "negative"), 
+    Example("The order was incorrect", "negative"), 
+    Example("I want to return my item", "negative"), 
+    Example("The item\'s material feels low quality", "negative"), 
+    Example("The product was okay", "neutral"), 
+    Example("I received five items in total", "neutral"), 
+    Example("I bought it from the website", "neutral"), 
+    Example("I used the product this morning", "neutral"), 
+    Example("The product arrived yesterday", "neutral"),
+]
+
+inputs=[
+  "This item was broken when it arrived",
+  "The product is amazing",
+  "The product was not too bad",
+]
+
+response = co.classify(
+  model='large',
+  inputs=inputs,
+  examples=examples,
+)
+
+print(response.classifications)
